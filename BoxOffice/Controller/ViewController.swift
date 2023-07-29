b56b4088b1475cc68e5ae9e856d94e2613bcf7be
JusBug <pksgwo123@gmail.com>
2023-07-29 23:20:06 +0900

    feat: UICollectionView 추가 및 ViewController에 연결

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -7,7 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? UICollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
