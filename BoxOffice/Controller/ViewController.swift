8aea6d5d71d4cb46e6daf30bcfe9ee355faae071
JusBug <pksgwo123@gmail.com>
2023-07-30 01:40:58 +0900

    feat: CustomCell의 xib를 CollectionView에 등록해주는 registerXib() 구현

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         super.viewDidLoad()
         self.CollectionView.dataSource = self
         self.CollectionView.delegate = self
+        registerXib()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -21,11 +22,21 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? UICollectionViewCell else {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath) as? CustomCell else {
             return UICollectionViewCell()
         }
+            
+        cell.rankLabel.text = "123"
+        cell.oldAndNewLabel.text = "New"
+        cell.movieName.text = "미션 임파서블"
+        cell.auditNumber.text = "12345"
         
         return cell
+    }
+    
+    private func registerXib() {
+        let nibName = UINib(nibName: "CustomCell", bundle: nil)
+        CollectionView.register(nibName, forCellWithReuseIdentifier: "customCell")
     }
 }
 
