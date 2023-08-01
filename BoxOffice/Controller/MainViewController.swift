ec54a937003f44def336f86b203ce44b486b7f88
redmango1446@gmail.com <redmango1446>
2023-08-01 15:57:16 +0900

    feat: 임시적으로 출력물 알아볼수 있게 cell크기 변경

--- BoxOffice/Controller/MainViewController.swift
+++ BoxOffice/Controller/MainViewController.swift
@@ -18,18 +18,18 @@ class MainViewController: UIViewController {
         collectionView.dataSource = self
         
         collectionView.register(UINib(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CollectionViewCell")
-//        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
     }
 }
 
 extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        4
+        1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
-        cell.backgroundColor = UIColor.blue // 셀의 배경색 설정 (임시로 파란색으로 설정)
+        
         return cell
     }
+    
 }
