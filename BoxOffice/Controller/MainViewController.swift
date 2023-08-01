8450c195ed75e8b09f0d224a8d089d852c844a11
redmango1446@gmail.com <redmango1446>
2023-08-01 17:02:00 +0900

    feat: 컬렉션 뷰 생성 및 추가

---
+++ BoxOffice/Controller/MainViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  BoxOffice
+//
+//  Created by kjs on 13/01/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemBackground
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        view.addSubview(collectionView)
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
+}
