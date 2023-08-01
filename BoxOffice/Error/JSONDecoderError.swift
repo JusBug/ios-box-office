bc43eec32412d031303dbbc44971833ffd2a4368
JusBug <pksgwo123@gmail.com>
2023-08-02 05:12:25 +0900

    refactor: APIService 타입 수정

---
+++ BoxOffice/Error/JSONDecoderError.swift
@@ -0,0 +1,10 @@
+//
+//  JSONDecoderError.swift
+//  BoxOffice
+//
+//  Created by 박종화 on 2023/08/02.
+//
+
+enum JSONDecoderError: Error {
+    case decodingError
+}
--- BoxOffice/Model/APIService.swift
+++ BoxOffice/Model/APIService.swift
@@ -11,20 +11,13 @@ enum APIService {
     case dailyBoxOffice
     case movieDetailInfo
     
-    func configureURL() -> URL? {
-        let dateFormatter = DateFormatter()
-        var components = URLComponents()
-        let targetDate = URLQueryItem(name: "targetDt", value: "20230731")
-
-        dateFormatter.dateFormat = "yyyy-mm-dd"
-        components.scheme = "https"
-        components.host = "www.kobis.or.kr"
-        components.path = "/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
-        components.queryItems = [targetDate]
-        
-        let url = components.url
-        
-        return url
+    func configureURL() {
+        switch self {
+        case .dailyBoxOffice:
+            print("")
+        case .movieDetailInfo:
+            print("")
+        }
     }
     
     var url: String {
@@ -35,13 +28,22 @@ enum APIService {
             return "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=9edeb739e275f3013ffb896c2ff41cfe&targetDt=20230724"
         }
     }
-    
-    var type: Any {
-        switch self {
-        case .dailyBoxOffice:
-            return BoxOffice.self
-        case .movieDetailInfo:
-            return Movie.self
-        }
+}
+
+extension APIService {
+    func setURLComponenst() -> URL? {
+        let dateFormatter = DateFormatter()
+        var components = URLComponents()
+        let targetDate = URLQueryItem(name: "targetDt", value: "20230731")
+        
+        dateFormatter.dateFormat = "yyyy-mm-dd"
+        components.scheme = "https"
+        components.host = "www.kobis.or.kr"
+        components.path = "/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+        components.queryItems = [targetDate]
+        
+        let url = components.url
+        
+        return url
     }
 }
