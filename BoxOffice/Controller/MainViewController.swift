8405f0cac55f789cc2a8a0343545c80c6da82a8c
JusBug <pksgwo123@gmail.com>
2023-08-15 15:20:30 +0900

    feat: UI 컴포넌트 생성 및 UI 연동

--- BoxOffice/Controller/MainViewController.swift
+++ BoxOffice/Controller/MainViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  BoxOffice
 //
-//  Created by kjs on 13/01/23.
+//  Created by redmango, Jusbug on 13/01/23.
 //
 
 import UIKit
---
+++ BoxOffice/Controller/MovieViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MovieViewController.swift
+//  BoxOffice
+//
+//  Created by redmango, Jusbug on 2023/08/15.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var directorLabel: UILabel!
+    @IBOutlet weak var productionYearLabel: UILabel!
+    @IBOutlet weak var openDate: UILabel!
+    @IBOutlet weak var showTime: UILabel!
+    @IBOutlet weak var watchGradeLabel: UILabel!
+    @IBOutlet weak var nationLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var actorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
