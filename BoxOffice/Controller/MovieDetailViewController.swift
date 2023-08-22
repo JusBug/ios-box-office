db0ab9583823f3d96ede8b8b63ae3824db1f7d73
JusBug <pksgwo123@gmail.com>
2023-08-22 21:14:21 +0900

    feat: UI 컴포넌트 및 연동

---
+++ BoxOffice/Controller/MovieDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MovieDetailViewController.swift
+//  BoxOffice
+//
+//  Created by 박종화 on 2023/08/22.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+    @IBOutlet weak var directorLabel: UILabel!
+    @IBOutlet weak var productionYearLabel: UILabel!
+    @IBOutlet weak var openDateLabel: UILabel!
+    @IBOutlet weak var showTimeLabel: UILabel!
+    @IBOutlet weak var watchGradeLabel: UILabel!
+    @IBOutlet weak var nationLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var actorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
